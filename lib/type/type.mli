8b6f10e7cec1f342c7c45a095b971cd1618dafdd
Jordy Dickinson <jordy.dickinson@icloud.com>
2020-02-07 16:22:10 -0500

    Remove deriving variants from type.mli

--- lib/type/type.mli
+++ lib/type/type.mli
@@ -10,18 +10,22 @@ type t = private
       params: t list;
       ret: t;
     }
-[@@deriving equal, sexp_of, variants]
+[@@deriving equal, sexp_of]
+
+val void : t
+val bool : t
+val int64 : t
+val float : t
+val pointer : t -> t
+val array : t -> t
+val struct_ : (string * t) list -> t
+val fun_ : params:t list -> ret:t -> t
 
 val is_fun : t -> bool
-
 val ret_exn : t -> t
-
 val params : t -> t list option
-
 val params_or_error : t -> t list Or_error.t
-
 val params_exn : t -> t list
-
 val deref_exn : t -> t
 
 module Kind : sig
@@ -35,7 +39,15 @@ module Kind : sig
     | Array
     | Struct
     | Fun
-  [@@deriving equal, sexp_of, variants]
+  [@@deriving equal, sexp_of]
+
+  val void : t
+  val bool : t
+  val numeric : t
+  val pointer : t
+  val array : t
+  val struct_ : t
+  val fun_ : t
 
   val of_type : concrete -> t
 end
